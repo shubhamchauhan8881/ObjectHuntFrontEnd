9ce985d15a9d121c0fd06e31978f24ef7f2d00ef
Shubham Chauhan <elskerelvish@outlook.com>
2024-08-01 18:39:45 +0530

    shubham final ui

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,42 @@
 import {Outlet} from "react-router-dom";
 import PopUp from "./components/PopUp";
 import SettingsButton from "./components/SettingsButton";
+import Loader from "./components/Loader";
+import { useContext, useEffect, useRef } from "react";
+import GameContext from "./context/GameContext";
+
+import bgAudio from './assets/gameaudio.mp3';
 
 export const Layout = () => {
+    const {loading , musicVol, musicPlay} = useContext(GameContext)
+    
+    const audio = useRef()
+    audio.current?.setAttribute('loop', true)
+    audio.current?.setAttribute('autoplay', true)
+    audio.current?.setAttribute('muted', true)
+
+
+    useEffect(() =>{
+        console.log(musicPlay)
+        if(musicPlay){
+            audio.current.volume = musicVol/100;
+            audio.current?.play();
+            
+        }else{
+            audio.current?.pause();
+        }
+    }, [audio, musicPlay, musicVol])
+
     return (
-        <div className="relative h-screen max-w-md mx-auto">
+        <div className="relative h-screen w-full sm:max-w-md mx-auto">
             <Outlet />
         
+
             {/* opop */}
             <PopUp/>
             <SettingsButton/>
+            { loading && <Loader/> }
+            <audio src={bgAudio} ref={audio} hidden></audio>
         </div>
     )
 }
--- src/context/GameContextProvider.jsx
+++ src/context/GameContextProvider.jsx
@@ -2,14 +2,19 @@ import React,{useState} from 'react'
 import GameContext from './GameContext';
 
 export default function GameContextProvider({children}) {
-    const [musicVol, setMusicVol] = useState(25);
-    const [TouchmusicVol, setTouchMusicVol] = useState(25);
+    const [musicVol, setMusicVol] = useState(0);
+    const [musicPlay, setMusciPlay] = useState(false);
+
+    const [TouchmusicVol, setTouchMusicVol] = useState(0);
+    const [loading, setLoading] = useState(false);
 
     const context = {
         musicVol,
         setMusicVol,
         TouchmusicVol,
         setTouchMusicVol,
+        musicPlay, setMusciPlay,
+        loading, setLoading
     }
   return (
     <>
