b620521b37002583a8d1bf86cbce2ef53f4ffdae
itzzAdnan56 <mohammadadnan5674@gmail.com>
2024-08-20 01:18:00 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className='flex flex-col items-center justify-content-center m-30'>
         <div>Crafted with <span className="text-red-500 animate-heartbeat">❤️</span> </div>
         <div className='divider px-8'>By:</div>
-        <p className='px-12 text-center font-medium'>Adarsh Kushwaha, Md. Adnan, Prashant Pandey, Shbubham Chauhan, Vinay Kumar, Vishnu Rajbhar</p>
+        <p className='px-12 text-center font-medium'>Adarsh Kushwaha, Mohammad Adnan, Prashant Pandey, Shubham Chauhan, Vinay Kumar, Vishnu Rajbhar</p>
         <Link to="/about">
           <button className="btn btn-outline btn-sm mt-4">About</button>
         </Link>
